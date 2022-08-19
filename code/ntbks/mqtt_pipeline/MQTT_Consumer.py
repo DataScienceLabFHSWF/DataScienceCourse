4f6d8ab1d4304fe220dd8f399fb22e50f49abecc
Felix Neubürger <fneubuerger@protonmail.com>
2022-08-19 12:44:34 +0200

    added correct preprocessing code in py file

--- code/ntbks/mqtt_pipeline/MQTT_Consumer.py
+++ code/ntbks/mqtt_pipeline/MQTT_Consumer.py
@@ -11,24 +11,34 @@ org = "FHSWF"
 bucket = "weather"
 token="clmwcDxm_39ML529Q5lbtT4XRLChNqqGull5VJt_zrFkAw4rTTPcMyzkhPo2sAlDhr6mIJW66en1PqRGmybRXQ=="
 
-def precip1h(i):
-    try: 
-        return weather_data['properties']['timeseries'][i]['data']['next_1_hours']['details']
-    except: 
-        return {'precipitation_amount': None}
+
+def precip_prob(i):
+    try:
+        return weather_data['properties']['timeseries'][i]['data']['next_1_hours']['details']['probability_of_precipitation']
+    except:
+        None
+def precip_amount(i):
+    try:
+        return weather_data['properties']['timeseries'][i]['data']['next_1_hours']['details']['precipitation_amount']
+    except:
+        None
+
 # define the preprocessing function returning a pandas df
 def format_data(weather_data):
     data = [weather_data['properties']['timeseries'][i]['data']['instant']['details'] for i in range(len(weather_data['properties']['timeseries']))]
     times = [weather_data['properties']['timeseries'][i]['time'] for i in range(len(weather_data['properties']['timeseries']))]
+    precip_proba = [precip_prob(i) for i in range(len(weather_data['properties']['timeseries']))]
+    precip = [precip_amount(i) for i in range(len(weather_data['properties']['timeseries']))]
     df = pd.DataFrame(data, index=times)
     df.index = pd.to_datetime(df.index)
     df.index.name = 'forecast_time'
     df['lat'] = weather_data['geometry']['coordinates'][1]
     df['lon'] = weather_data['geometry']['coordinates'][0]
-    df['height'] =weather_data['geometry']['coordinates'][2]
+    df['height'] = weather_data['geometry']['coordinates'][2]
     df['timestamp'] = weather_data['properties']['meta']['updated_at']
-    #precipitation1h = pd.DataFrame([precip1h(i) for i in range(len(weather_data['properties']['timeseries']))], index=times)
-    #df = df.join(precipitation1h)
+    df['precipitation_prob'] = precip_proba
+    df['precipitation_amount'] = precip
+
     return df
 
 
